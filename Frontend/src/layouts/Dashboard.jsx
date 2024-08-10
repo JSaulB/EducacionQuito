e1c408ca6743a4078908e80d556a9a88e8955df4
silviachaluisa <veronica-14-1998@hotmail.com>
2024-08-10 11:18:59 -0500

    Menu Principal

--- Frontend/src/layouts/Dashboard.jsx
+++ Frontend/src/layouts/Dashboard.jsx
@@ -20,11 +20,8 @@ export const Dashboard = () => {
                 <p className='text-slate-400 text-center my-4 text-sm'> <span className='bg-red-600 w-3 h-3 inline-block rounded-full'></span> Modo - {auth?.rol}</p>
                 <hr className="mt-5 border-slate-500" />
 
-                <ul className="mt-5">
-
-                    <li className="text-center">
                         
-                            <form>
+               <form>
 
                 <div> 
                     <h1 className='font-black text-4xl text-gray-900'>Mi Perfil</h1>
@@ -40,75 +37,56 @@ export const Dashboard = () => {
                         id='nombre'
                         type="text"
                         className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
-                        placeholder='nombre'
-                        name='nombre'
+                        placeholder='Nombre'
+                        name='Nombre'
                     />
                 </div>
                 <div>
                     <label
-                        htmlFor='nombre'
-                        className='text-gray-100 uppercase font-bold text-sm'>Nombre: </label>
+                        htmlFor='apellido'
+                        className='text-gray-100 uppercase font-bold text-sm'>Apellido: </label>
                     <input
-                        id='nombre'
+                        id='apellido'
                         type="text"
                         className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
-                        placeholder='nombre'
-                        name='nombre'
+                        placeholder='Apellido'
+                        name='Apellido'
                     />
                 </div>
                 <div>
                     <label
-                        htmlFor='nombre'
-                        className='text-gray-100 uppercase font-bold text-sm'>Nombre: </label>
+                        htmlFor='email'
+                        className='text-gray-100 uppercase font-bold text-sm'>Email: </label>
                     <input
-                        id='nombre'
+                        id='email'
                         type="text"
                         className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
-                        placeholder='nombre'
-                        name='nombre'
+                        placeholder='Email'
+                        name='Email'
                     />
                 </div>
 
                 <div>
                     <label
-                        htmlFor='detalles'
-                        className='text-gray-100 uppercase font-bold text-sm'>Detalles: </label>
+                        htmlFor='modo'
+                        className='text-gray-100 uppercase font-bold text-sm'>Modo: </label>
                     <textarea
-                        id='detalles'
+                        id='modo'
                         type="text"
                         className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
-                        name='detalles'
+                        name='modo'
                     />
                 </div>
 
                 <input
                     type="submit"
                     className='bg-green-600 w-m p-3 
-            text-slate-300 uppercase font-bold rounded-lg 
-            hover:bg-gray-600 cursor-pointer transition-all'
+                    text-slate-300 uppercase font-bold rounded-lg 
+                    hover:bg-gray-600 cursor-pointer transition-all'
                     value='Actualizar' />
 
             </form>
-                        
-                    </li>
-                    <li className="text-center">
-                        <Link to='/dashboard/crearUsuario' className={`${urlActual === '/dashboard/crearUsuario' ? 'text-slate-100 bg-gray-900 px-3 py-2 rounded-md text-center' : 'text-slate-600'} text-xl block mt-2 hover:text-slate-600`}>Crear Usuarios</Link>
-                    </li>
-
-                    <li className="text-center">
-                        <Link to='/dashboard/listar' className={`${urlActual === '/dashboard/listar' ? 'text-slate-200 bg-gray-900 px-3 py-2 rounded-md text-center' : 'text-slate-600'} text-xl block mt-2 hover:text-slate-600`}>Lista de Usuarios</Link>
-                    </li>
-
-                    <li className="text-center">
-                        <Link to='/dashboard/crear' className={`${urlActual === '/dashboard/crear' ? 'text-slate-100 bg-gray-900 px-3 py-2 rounded-md text-center' : 'text-slate-600'} text-xl block mt-2 hover:text-slate-600`}>Registrar Instituciones</Link>
-                    </li>
-                    <li className="text-center">
-                        <Link to='/dashboard/listaInstituciones' className={`${urlActual === '/dashboard/listaInstituciones' ? 'text-slate-100 bg-gray-900 px-3 py-2 rounded-md text-center' : 'text-slate-600'} text-xl block mt-2 hover:text-slate-600`}>Lista de Instituciones</Link>
-                    </li>
-                    <li className="text-center">
-                        <Link to='/dashboard/actualizar' className={`${urlActual === '/dashboard/actualizar' ? 'text-slate-100 bg-gray-900 px-3 py-2 rounded-md text-center' : 'text-slate-600'} text-xl block mt-2 hover:text-slate-600`}>Actualizar Institución</Link>
-                    </li>
-                </ul>
+                   
 
             </div>
 
@@ -119,18 +97,16 @@ export const Dashboard = () => {
     
                     </div> }
                     
-                    {/* {<div>
-                        <img src="https://cdn-icons-png.flaticon.com/512/4715/4715329.png" alt="img-client" className="border-2 border-green-600 rounded-full" width={50} height={50} />
-                    </div> } */}
+            
                     { <div>
                         <Link to='/landingpage' className=" text-white mr-3 text-md block hover:bg-red-900 text-center
                         bg-red-800 px-4 py-1 rounded-lg">Salir</Link>
                     </div> }
                 </div>
-                <div className='overflow-y-scroll p-8'>
+                <div className='overflow-y-scroll p-2'>
                     <Outlet />
                 </div>
-                <div className='bg-gray-800 h-12'>
+                <div className='bg-blue-700 h-12'>
                     <p className='text-center  text-slate-100 leading-[2.9rem] underline'>Todos los derechos reservados</p>
                 </div>
 
--- Frontend/src/paginas/Perfil.jsx
+++ Frontend/src/paginas/Perfil.jsx
@@ -1,15 +1,75 @@
+import { Link, Navigate, Outlet, useLocation } from 'react-router-dom'
+import {useContext} from 'react'
+import AuthContext from "../context/AuthProvider";
+
 export const Perfil = () => {
+    const location = useLocation()
+    const urlActual = location.pathname
+    const {auth} = useContext(AuthContext)
+    console.log(auth)
     return (
 
         <form>
+            <h1 className='text-center text-white text-3xl font-bold font-serif mt-2 mb-40 '> Menú Principal</h1>
 
-            <div> 
+            <ul className="mt-5 flex flex-wrap justify-center gap-4">
+    <li className="bg-white p-4 rounded-lg shadow-md flex items-center justify-center" style={{ height: '150px', width: '150px' }}>
+        <Link 
+            to='/dashboard/crearUsuario' 
+            className={`${urlActual === '/dashboard/crearUsuario' ? 'text-slate-100 bg-gray-900 px-3 py-2 rounded-md' : 'text-slate-600'} text-xl block text-center hover:text-slate-600`}
+        >
+            Crear Usuarios
+        </Link>
+    </li>
+
+    <li className="bg-white p-4 rounded-lg shadow-md flex items-center justify-center" style={{ height: '150px', width: '150px' }}>
+        <Link 
+            to='/dashboard/listar' 
+            className={`${urlActual === '/dashboard/listar' ? 'text-slate-200 bg-gray-900 px-3 py-2 rounded-md' : 'text-slate-600'} text-xl block text-center hover:text-slate-600`}
+        >
+            Lista de Usuarios
+        </Link>
+    </li>
+
+    <li className="bg-white p-4 rounded-lg shadow-md flex items-center justify-center" style={{ height: '150px', width: '150px' }}>
+        <Link 
+            to='/dashboard/crear' 
+            className={`${urlActual === '/dashboard/crear' ? 'text-slate-100 bg-gray-900 px-3 py-2 rounded-md' : 'text-slate-600'} text-xl block text-center hover:text-slate-600`}
+        >
+            Registrar Instituciones
+        </Link>
+    </li>
+
+    <li className="bg-white p-4 rounded-lg shadow-md flex items-center justify-center" style={{ height: '150px', width: '150px' }}>
+        <Link 
+            to='/dashboard/listaInstituciones' 
+            className={`${urlActual === '/dashboard/listaInstituciones' ? 'text-slate-100 bg-gray-900 px-3 py-2 rounded-md' : 'text-slate-600'} text-xl block text-center hover:text-slate-600`}
+        >
+            Lista de Instituciones
+        </Link>
+    </li>
+
+    <li className="bg-white p-4 rounded-lg shadow-md flex items-center justify-center" style={{ height: '150px', width: '150px' }}>
+        <Link 
+            to='/dashboard/actualizar' 
+            className={`${urlActual === '/dashboard/actualizar' ? 'text-slate-100 bg-gray-900 px-3 py-2 rounded-md' : 'text-slate-600'} text-xl block text-center hover:text-slate-600`}
+        >
+            Actualizar Institución
+        </Link>
+    </li>
+</ul>
+
+
+
+
+
+            {/* <div> 
                 <h1 className='font-black text-4xl text-gray-100'>Perfil</h1>
                 <hr className='my-4' />
                 <p className='mb-8'>Visualizar Perfil</p>
-            </div>
+            </div> */}
 
-            <div>
+            {/* <div>
                 <label
                     htmlFor='nombre'
                     className='text-gray-100 uppercase font-bold text-sm'>Nombre: </label>
@@ -24,7 +84,7 @@ export const Perfil = () => {
             <div>
                 <label
                     htmlFor='nombre'
-                    className='text-gray-100 uppercase font-bold text-sm'>Nombre: </label>
+                    className='text-gray-100 uppercase font-bold text-sm'>Apellido: </label>
                 <input
                     id='nombre'
                     type="text"
@@ -36,7 +96,7 @@ export const Perfil = () => {
             <div>
                 <label
                     htmlFor='nombre'
-                    className='text-gray-100 uppercase font-bold text-sm'>Nombre: </label>
+                    className='text-gray-100 uppercase font-bold text-sm'>Email: </label>
                 <input
                     id='nombre'
                     type="text"
@@ -49,21 +109,23 @@ export const Perfil = () => {
             <div>
                 <label
                     htmlFor='detalles'
-                    className='text-gray-100 uppercase font-bold text-sm'>Detalles: </label>
+                    className='text-gray-100 uppercase font-bold text-sm'>Modo: </label>
                 <textarea
                     id='detalles'
                     type="text"
                     className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
                     name='detalles'
                 />
-            </div>
+            </div> */}
 
-            <input
-                type="submit"
-                className='bg-green-600 w-m p-3 
-        text-slate-300 uppercase font-bold rounded-lg 
-        hover:bg-gray-600 cursor-pointer transition-all'
-                value='Actualizar' />
+            
+
+                {/* <input
+                    type="submit"
+                    className='bg-green-600 w-m p-3 
+            text-slate-300 uppercase font-bold rounded-lg 
+            hover:bg-gray-600 cursor-pointer transition-all'
+                    value='Actualizar' /> */}
 
         </form>
     )
