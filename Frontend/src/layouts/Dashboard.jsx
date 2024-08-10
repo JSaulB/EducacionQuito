5fcb0758fa122fb2b71038b2954dba2d21a61428
silviachaluisa <veronica-14-1998@hotmail.com>
2024-08-10 01:03:08 -0500

    Mi Perfil

--- Frontend/src/layouts/Dashboard.jsx
+++ Frontend/src/layouts/Dashboard.jsx
@@ -13,17 +13,83 @@ export const Dashboard = () => {
 
             <div className='md:w-1/5 bg-gray-100 px-5 py-4'>
 
-                <h2 className='text-4xl font-black text-center text-slate-200'>BIENVENIDO</h2>
+                <h2 className='text-4xl font-black text-center text-slate-900'>BIENVENIDO</h2>
 
                 <img src="https://cdn-icons-png.freepik.com/512/16/16480.png" alt="img-client" className="m-auto mt-8 p-1 border-2 border-slate-500 rounded-full" width={120} height={120} />
-                <p className='text-slate-400 text-center my-4 text-sm'> <span className='bg-green-600 w-3 h-3 inline-block rounded-full'></span> Bienvenido - {auth?.nombre}</p>
-                <p className='text-slate-400 text-center my-4 text-sm'> <span className='bg-red-600 w-3 h-3 inline-block rounded-full'></span> Rol - {auth?.rol}</p>
+                <p className='text-slate-400 text-center my-4 text-sm'> <span className='bg-green-600 w-3 h-3 inline-block rounded-full'></span> Hola - {auth?.nombre}</p>
+                <p className='text-slate-400 text-center my-4 text-sm'> <span className='bg-red-600 w-3 h-3 inline-block rounded-full'></span> Modo - {auth?.rol}</p>
                 <hr className="mt-5 border-slate-500" />
 
                 <ul className="mt-5">
 
                     <li className="text-center">
-                        <Link to='/dashboard' className={`${urlActual === '/dashboard' ? 'text-slate-200 bg-gray-900 px-3 py-2 rounded-md text-center' : 'text-slate-600'} text-xl block mt-2 hover:text-slate-600`}>Perfil</Link>
+                        
+                            <form>
+
+                <div> 
+                    <h1 className='font-black text-4xl text-gray-900'>Mi Perfil</h1>
+                    <hr className='my-4' />
+        
+                </div>
+
+                <div>
+                    <label
+                        htmlFor='nombre'
+                        className='text-gray-100 uppercase font-bold text-sm'>Nombre: </label>
+                    <input
+                        id='nombre'
+                        type="text"
+                        className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                        placeholder='nombre'
+                        name='nombre'
+                    />
+                </div>
+                <div>
+                    <label
+                        htmlFor='nombre'
+                        className='text-gray-100 uppercase font-bold text-sm'>Nombre: </label>
+                    <input
+                        id='nombre'
+                        type="text"
+                        className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                        placeholder='nombre'
+                        name='nombre'
+                    />
+                </div>
+                <div>
+                    <label
+                        htmlFor='nombre'
+                        className='text-gray-100 uppercase font-bold text-sm'>Nombre: </label>
+                    <input
+                        id='nombre'
+                        type="text"
+                        className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                        placeholder='nombre'
+                        name='nombre'
+                    />
+                </div>
+
+                <div>
+                    <label
+                        htmlFor='detalles'
+                        className='text-gray-100 uppercase font-bold text-sm'>Detalles: </label>
+                    <textarea
+                        id='detalles'
+                        type="text"
+                        className='border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md mb-5'
+                        name='detalles'
+                    />
+                </div>
+
+                <input
+                    type="submit"
+                    className='bg-green-600 w-m p-3 
+            text-slate-300 uppercase font-bold rounded-lg 
+            hover:bg-gray-600 cursor-pointer transition-all'
+                    value='Actualizar' />
+
+            </form>
+                        
                     </li>
                     <li className="text-center">
                         <Link to='/dashboard/crearUsuario' className={`${urlActual === '/dashboard/crearUsuario' ? 'text-slate-100 bg-gray-900 px-3 py-2 rounded-md text-center' : 'text-slate-600'} text-xl block mt-2 hover:text-slate-600`}>Crear Usuarios</Link>
@@ -47,19 +113,19 @@ export const Dashboard = () => {
             </div>
 
             <div className='flex-1 flex flex-col justify-between h-screen'>
-                <div className='bg-gray-800 py-2 flex md:justify-end items-center gap-5 justify-center'>
-                    <div className='text-md font-semibold text-slate-100'>
+                <div className='bg-gray-100 py-2 flex md:justify-end items-center gap-5 justify-center'>
+                    {/* <div className='text-md font-semibold text-slate-100'>
                         Usuario - {auth?.nombre}
     
-                    </div>
+                    </div> */}
                     
-                    <div>
+                    {/* <div>
                         <img src="https://cdn-icons-png.flaticon.com/512/4715/4715329.png" alt="img-client" className="border-2 border-green-600 rounded-full" width={50} height={50} />
-                    </div>
-                    <div>
+                    </div> */}
+                    {/* <div>
                         <Link to='/landingpage' className=" text-white mr-3 text-md block hover:bg-red-900 text-center
                         bg-red-800 px-4 py-1 rounded-lg">Salir</Link>
-                    </div>
+                    </div> */}
                 </div>
                 <div className='overflow-y-scroll p-8'>
                     <Outlet />
